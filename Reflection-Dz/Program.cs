2a34511c68fc4feb004ab2ec1783d44f7d706ccf
JekaLion <djoni-01@mail.ru>
2017-12-28 17:12:42 +0600

    init

---
+++ Reflection-Dz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            Type type = typeof(Console);
+
+            var memebersInfo = type.GetMethods();
+
+            foreach (var info in memebersInfo)
+            {
+                Console.WriteLine(info);
+            }
+
+            #endregion
+            Console.ReadLine();
+
+            #region Task2
+            User user = new User("Aleksey Popov", "LexaSuper", "qwerty123");
+            Type typeUser = typeof(User);
+
+            var memebersInfoUser = typeUser.GetProperties();
+
+            for (int i = 0; i < memebersInfoUser.Length; i++)
+            {
+                Console.WriteLine(memebersInfoUser[i] + " " + memebersInfoUser[i].GetValue(user));
+            }
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reflection-Dz/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Dz
+{
+    public class User
+    {
+        public string FullName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public User(string fullName, string login, string password)
+        {
+            FullName = fullName;
+            Login = login;
+            Password = password;
+        }
+        public User()
+        {
+            //
+        }
+    }
+}
